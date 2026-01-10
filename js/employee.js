2790fbcc73f0eb2e9a7f9fc6f9fc2b3d82ffe6b9
Preeti Lohia <preetilohmod35@gmail.com>
2026-01-10 18:05:18 +0530

    Complete Employee frontend prototype

---
+++ js/employee.js
@@ -0,0 +1,8 @@
+// Mock employee data (backend later replace karega)
+const employee = {
+    name: "Rahul Kumar",
+    department: "Sanitation",
+    designation: "Supervisor"
+};
+
+document.getElementById("empName").innerText = employee.name;
