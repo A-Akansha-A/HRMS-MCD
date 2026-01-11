31fb8364837234383f7622cdd88a185897a6d956
Tannu <tannu.tannu.dev.@gmail.com>
2026-01-11 22:28:06 +0530

    Added Grievance Redressal module

---
+++ frontend/dashboard/admin/grievances.js
@@ -0,0 +1,11 @@
+function markResolved() {
+  alert("Grievance marked as Resolved (Demo)");
+}
+
+function startProgress() {
+  alert("Grievance moved to In Progress (Demo)");
+}
+
+function submitResolution() {
+  alert("Resolution submitted successfully (Demo)");
+}
